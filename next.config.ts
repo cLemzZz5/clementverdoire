8bf3adaaebe8d4453cd42c31e4febaaafe8ff4a1
clement.verdoire <clem.v08@gmail.com>
2025-06-26 11:41:49 +0200

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    basePath: '/containers/clementverdoire-portfolio-v2',
+    basePath: '/containers/clementverdoire-portfolio-v2/',
     assetPrefix: '/containers/clementverdoire-portfolio-v2/', // utile pour charger les images, fonts, etc.
     experimental: {
         outputFileTracingIncludes: {
